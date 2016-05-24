b73ba42b93a2915e3189b760870b8c069f047779
iiksamedo <iiksa_medo@hotmail.com>
2016-05-24 23:43:15 +0300

    Update config.php

--- TimeBot-Style2/config.php
+++ TimeBot-Style2/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$ts3_ip = 'localhost'; #IP Adress
+$ts3_ip = '191.101.230.177'; #IP Adress
 
 $ts3_queryport = 10011 ; #Queryport 
 
@@ -8,17 +8,17 @@ $ts3_port = 9987; #Port
 
 $ts3_user = 'serveradmin'; #Login Query
 
-$ts3_pass = 'pass'; #Password Query
+$ts3_pass = 'vC0ZpLI1'; #Password Query
 
 $bot_nickname = 'TimeBot' #Name Bot
 
-$bot_move = 5 ; #ID Channel to move
+$bot_move = 1842 ; #ID Channel to move
 
-$channel_id_1 = 1 ; #ID Channel time 1
+$channel_id_1 = 1843 ; #ID Channel time 1
 
-$channel_id_2 = 2 ; #ID Channel time 2
+$channel_id_2 = 1844 ; #ID Channel time 2
 
-$channel_id_3 = 3 ; #ID Channel time 2
+$channel_id_3 = 1845 ; #ID Channel time 2
 
 
 ?>
